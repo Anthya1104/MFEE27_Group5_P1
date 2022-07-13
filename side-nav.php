92fa296ae466f7c994bb2674ae8486a3351da290
Jiayu <justine0816123@gmail.com>
2022-07-13 15:31:50 +0800

    update side-nav

--- side-nav.php
+++ side-nav.php
@@ -1,22 +1,29 @@
 
-  <div class="d-flex flex-column flex-shrink-0 p-3 text-white position-fixed vh-100" style="width:300px; background:#102e2ef8;">
-    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-      <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-      <span class="" style="font-size:14px">缺eBook</span>
+
+  <div class="position-relative d-flex flex-column flex-shrink-0 p-3 text-white position-fixed vh-100" style="width:300px; background:#102e2ef8;">
+    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none ">
+    <svg class="bi me-2" width="16" height="16"><use xlink:href="#home"></use></svg>
+
+          <figure class="my-4 position-absolute" style="width:150px; left:45px; top:5px;" >
+            <img src="/MFEE27_Group5_P1/2.png" alt=""  style="width:100%; height:100%; object-fit:cover;">
+          </figure>
+          <!-- <span class="" style="font-size:30px">  &nbsp 缺eBook</span> -->
     </a>
-    <hr>
-    <ul class="nav nav-pills flex-column mb-auto">
+
+    <!-- <hr> -->
+    <ul class="nav nav-pills flex-column mb-auto" style="margin-top:150px;">
       <li class="nav-item">
-        <a href="#" class="nav-link text-white" style="font-size:14px">
+        <a href="#" class="nav-link text-white" style="font-size:20px">
           <svg class="bi me-2" width="16" height="16"><use xlink:href="#home"></use></svg>
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16">
           <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z"/>
-          </svg>
+          </svg>    
+
           &nbspHome
         </a>
       </li>
       <li>
-        <a href="/MFEE27_Group5_P1/factory/factory_index.php" class="nav-link text-white" style="font-size:14px">
+        <a href="/MFEE27_Group5_P1/factory/factory_index.php" class="nav-link text-white" style="font-size:20px">
           <svg class="bi me-2" width="16" height="16"><use xlink:href="#speedometer2"></use></svg>
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
           <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
@@ -25,7 +32,7 @@
         </a>
       </li>
       <li>
-        <a href="/MFEE27_Group5_P1/product-create/product-list.php" class="nav-link text-white" style="font-size:14px">
+        <a href="/MFEE27_Group5_P1/product-create/product-list.php" class="nav-link text-white" style="font-size:20px">
           <svg class="bi me-2" width="16" height="16"><use xlink:href="#table"></use></svg>
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-fill" viewBox="0 0 16 16">
           <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5z"/>
@@ -34,7 +41,7 @@
         </a>
       </li>
       <li>
-        <a href="/MFEE27_Group5_P1/order_system/user_order.php" class="nav-link text-white" style="font-size:14px">
+        <a href="/MFEE27_Group5_P1/order_system/user_order.php" class="nav-link text-white" style="font-size:20px">
           <svg class="bi me-2" width="16" height="16"><use xlink:href="#grid"></use></svg>
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-layout-text-sidebar" viewBox="0 0 16 16">
           <path d="M3.5 3a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zm0 3a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zM3 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5z"/>
@@ -44,7 +51,7 @@
         </a>
       </li>
       <li>
-        <a href="#" class="nav-link text-white" style="font-size:14px">
+        <a href="#" class="nav-link text-white" style="font-size:20px">
           <svg class="bi me-2" width="16" height="16"><use xlink:href="#people-circle"></use></svg>
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar-fill" viewBox="0 0 16 16">
           <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V5h16V4H0V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5z"/>
@@ -54,7 +61,7 @@
       </li>
       <hr>
       <li>
-        <a href="/MFEE27_Group5_P1/member-comment/comment-list.php" class="nav-link text-white" style="font-size:14px">
+        <a href="/MFEE27_Group5_P1/member-comment/comment-list.php" class="nav-link text-white" style="font-size:20px">
           <svg class="bi me-2" width="16" height="16"><use xlink:href="#table"></use></svg>
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
           <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
