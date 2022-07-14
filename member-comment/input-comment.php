7bebe59e4d3efdfbbce65fde113714b806ddd94b
Yusa <anthya1104@gmail.com>
2022-07-14 10:54:01 +0800

    input-comment 1 more book

--- member-comment/input-comment.php
+++ member-comment/input-comment.php
@@ -5,7 +5,7 @@ if(!isset($_SESSION["user"])){
 }
 require("../db-connect.php");
 //抓出book的資訊
-$id=2;
+$id=6;
 $sql="SELECT * FROM product WHERE id=$id";
 $result=$conn->query($sql);
 $rows=$result->fetch_assoc();
@@ -37,43 +37,47 @@ $member_id=$_SESSION["user"]["id"];
       <div class="text-center py-2">      
         <h1 class="comment-h1">Welcome, <?=$_SESSION["user"]["account"]?></h1>
       </div>
-        <div class="d-flex justify-content-center my-2">
-          <div class="row">
-            <div class="col-12">
-              <figure class="text-center">
-                <img src="../product-create/image/book<?=$id?>.jpg" alt="book<?=$id?>">
-              </figure>
+      <div class="d-flex justify-content-center my-2">
+        <div class="row">
+          <div class="col-12">
+            <figure class="text-center">
+              <img src="../product-create/image/book<?=$id?>.jpg" alt="book<?=$id?>">
+            </figure>
 
-            </div>
-            <div class="col-12 comment">
-              <div class="row mt-3">
-                <form action="comment-do-create.php" method="post">
-                <input name="member_id" type="hidden" value="<?=$member_id?>">
-                <input name="book_id" type="hidden" value="<?=$id?>">
+      </div>
+      <div class="col-12 comment">
+        <div class="row mt-3">
+          <form action="comment-do-create.php" method="post">
+            <input name="member_id" type="hidden" value="<?=$member_id?>">
+            <input name="book_id" type="hidden" value="<?=$id?>">
 
-                <!-- $_SESSION["user"]["id"]->member->id   $id-> product->id -->
-                <?php //var_dump($_SESSION["user"]["id"].$id);?>
-                    <div class="form-group">
-                      <h3><?=$_SESSION["user"]["username"]?></h3><br><br>
-                        <h4 class="py-2">留下評論</h4>
-                        
-                        <textarea name="comments" id="comments" cols="30" rows="5" class="form-control mt-3" ></textarea>
-                    </div>
-                    <div class="form-group my-3">
-                        <button type="submit" class="btn btn-info">送出評論</button>    
+            <!-- $_SESSION["user"]["id"]->member->id   $id-> product->id -->
+            <?php //var_dump($_SESSION["user"]["id"].$id);?>
+              <div class="form-group">
+                <h3><?=$_SESSION["user"]["username"]?></h3><br><br>
+                  <h4 class="py-2">留下評論</h4>
+                          
+                    <textarea name="comments" id="comments" cols="30" rows="5" class="form-control mt-3" ></textarea>
+          </div>
+          <div class="form-group my-3">
+              <button type="submit" class="btn btn-info">送出評論</button>    
 
-                    </div>
-                </form>                        
+          </div>
+        </form>  
+                
 
-            </div>
-                <a class="btn btn-info mb-2" href="../session-destroy.php" >登出</a>
-            </div>
+          <div >
+            <a class="btn btn-info mb-2" href="./input-comment-2.php" >下一本書</a>
+
+          </div>
+          <div >
+            <a class="btn btn-info mb-2" href="../session-destroy.php" >登出</a>
+
           </div>
 
+      </div>
 
-        </div>
 
-    </div>
 
     </section>
   </body>
